14c818f8ebc937a556b180ac7988066a7224b621
armdev <armen.arzumanyan@gmail.com>
2018-04-17 23:20:05 +0400

    onBackpressureDrop()

---
+++ sp29/readme.md
@@ -0,0 +1,5 @@
+1. Run project as spring boot application
+2. http://localhost:10008/api/v1/data/gen  //event will be dropped automatically
+3. http://localhost:10008/actuator
+4. http://localhost:10008/actuator/health
+5. http://localhost:10008/swagger-ui.html
---
+++ sp29/src/main/java/io/project/sp/resources/MainController.java
@@ -0,0 +1,51 @@
+package io.project.sp.resources;
+
+import java.math.BigDecimal;
+import java.time.Duration;
+import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+/**
+ *
+ * @author armdev
+ */
+@RestController
+@RequestMapping(path = "/api/v1/data", produces = MediaType.APPLICATION_JSON_VALUE)
+public class MainController {
+
+    private final Random random = new Random();
+
+    private final List<Double> prices = new ArrayList<>();
+
+    @GetMapping(path = "/gen", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Flux<Double> get() {
+        return fetchStream(Duration.ofSeconds(1));
+    }
+
+    public Flux<Double> fetchStream(Duration period) {
+
+      
+        return Flux.interval(period)
+                // In case of back-pressure, drop events
+                .onBackpressureDrop()
+                // For each tick, generate a list of quotes
+                .map(this::generateData)
+                .flatMapIterable(q -> q)
+                .log("Generating data");
+    }
+
+    private List<Double> generateData(long interval) {    
+
+        return prices.stream().map(b -> this.random.nextDouble()).collect(Collectors.toList());
+
+    }
+
+}
