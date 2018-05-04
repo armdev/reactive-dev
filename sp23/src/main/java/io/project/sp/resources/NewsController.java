6523457e1d87dfda84f53587714741b331526789
armdev <armen.arzumanyan@gmail.com>
2018-05-05 00:27:43 +0400

    new scheme

--- sp23/src/main/java/io/project/sp/resources/NewsController.java
+++ sp23/src/main/java/io/project/sp/resources/NewsController.java
@@ -1,5 +1,8 @@
 package io.project.sp.resources;
 
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +15,34 @@ import reactor.core.publisher.Mono;
  * @author armdev
  */
 @RestController
-@RequestMapping(path="/api/v1/news", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping(path = "/api/v1/news", produces = MediaType.APPLICATION_JSON_VALUE)
 public class NewsController {
+
+    private List<User> userList = new ArrayList<>();
+
+    @GetMapping(path = "/users", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Mono<List<User>> getUsers() {
+        User user = new User();
+        user.setId("1");
+        user.setUsername("aa");
+        user.setPassword("123456");
+        userList.add(user);
+        user.setId("2");
+        user.setUsername("saa");
+        user.setPassword("1sss23456");
+        userList.add(user);
+        user.setId("3");
+        user.setUsername("sfsfaa");
+        user.setPassword("1sssfdfd23456");
+        userList.add(user);
+
+       return Flux.fromIterable(userList).collectList();
+    }
 
     @GetMapping(path = "/top", produces = MediaType.APPLICATION_JSON_VALUE)
     public Flux<String> getTopNews() {
         return Flux.just("Mr. Zuckerberg goes to Washington — so let’s stop acting like he can’t handle it. He can.",
-                "Facing trade war with the U.S., China’s Xi vows to open auto industry", 
+                "Facing trade war with the U.S., China’s Xi vows to open auto industry",
                 "Here’s why owning a bike-share service makes sense for Uber");
     }
 
---
+++ sp23/src/main/java/io/project/sp/resources/User.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package io.project.sp.resources;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ *
+ * @author Admin
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    private String id;
+    private String username;
+    private String password;
+}
