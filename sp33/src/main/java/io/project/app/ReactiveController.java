9d45822c799c78383da8203da4e1d8473ca269be
armena <armen.arzumanyan@optym.com>
2019-01-21 16:36:01 +0400

    next best push, swagger

--- sp33/src/main/java/io/project/app/ReactiveController.java
+++ sp33/src/main/java/io/project/app/ReactiveController.java
@@ -1,5 +1,6 @@
 package io.project.app;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,22 +15,29 @@ import reactor.core.publisher.Mono;
  */
 @RestController
 @RequestMapping("/api/v2/users")
+@Slf4j
 public class ReactiveController {
 
-    // http://localhost:8585/api/v2/users/?name=Yerevan
+    
     @GetMapping
     public Mono<String> find(@RequestParam String name) {
-        return Mono.just("Hello " + name);
+        log.info("We are loving coding in production mode!");
+        return Mono.just("Hello " + name + " I hope I am reactive application!!!!!");
     }
 
     @GetMapping("/user")
-    public ResponseEntity<Mono<?>> get() {
+    public ResponseEntity<Mono<?>> get() {        
+        log.info("This is a GET request from you");
         return new ResponseEntity<>(Mono.just("One User here"), HttpStatus.I_AM_A_TEAPOT);
     }
 
     @GetMapping("/user/second")
     public Mono<ResponseEntity<?>> load() {
-        return Mono.just(ResponseEntity.ok().body("Second user here"));
+        log.info("Load me reactive client!");
+        return Mono.just(ResponseEntity.ok().body("Second user location here"));
     }
 
 }
+
+
+// call me  http://localhost:8585/api/v2/users/?name=Yerevan
--- sp33/src/main/java/io/project/app/SwaggerConfig.java
+++ sp33/src/main/java/io/project/app/SwaggerConfig.java
@@ -13,7 +13,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 /**
  *
- * @author armena
+ * @author armenacd
  */
 @Configuration
 @EnableSwagger2WebFlux
